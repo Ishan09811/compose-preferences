8c242025f5bc2b75d2ac9a5dcfc2b2ea5a97076d
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2026-05-06 01:20:13 +0530

    SwitchPreference: simulate click on switch to get accurate animation

--- compose-preferences/src/main/java/com/github/ishan09811/compose_preferences/preference/SwitchPreference.kt
+++ compose-preferences/src/main/java/com/github/ishan09811/compose_preferences/preference/SwitchPreference.kt
@@ -1,5 +1,7 @@
 package com.github.ishan09811.compose_preferences.preference
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SwitchDefaults
@@ -8,18 +10,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import com.github.ishan09811.materialswitch.MaterialSwitch
-import com.github.ishan09811.materialswitch.MaterialSwitchColors
 import com.github.ishan09811.compose_preferences.R
-import com.github.ishan09811.compose_preferences.util.ComposePreview
 import com.github.ishan09811.compose_preferences.core.PreferenceIcon
 import com.github.ishan09811.compose_preferences.core.PreferenceSubtitle
 import com.github.ishan09811.compose_preferences.core.PreferenceTitle
+import com.github.ishan09811.compose_preferences.util.ComposePreview
+import com.github.ishan09811.materialswitch.MaterialSwitch
+import com.github.ishan09811.materialswitch.MaterialSwitchColors
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun SwitchPreference(
@@ -36,6 +42,8 @@ fun SwitchPreference(
     onClick: (Boolean) -> Unit,
     onLongClick: () -> Unit = {}
 ) {
+    val scope = rememberCoroutineScope()
+    val interactionSource = remember { MutableInteractionSource() }
     val onValueUpdated: (Boolean) -> Unit = { newValue -> onClick(newValue) }
     RegularPreference(
         title = title,
@@ -47,11 +55,23 @@ fun SwitchPreference(
                 checked = checked,
                 onCheckedChange = { onValueUpdated(it) },
                 enabled = enabled,
-                colors = switchColors
+                colors = switchColors,
+                interactionSource = interactionSource
             )
         },
         enabled = enabled,
-        onClick = { onValueUpdated(!checked) },
+        onClick = {
+            scope.launch {
+                val press = PressInteraction.Press(Offset.Zero)
+                interactionSource.emit(press)
+
+                delay(50)
+
+                interactionSource.emit(PressInteraction.Release(press))
+
+                onValueUpdated(!checked)
+            }
+        },
         onLongClick = onLongClick
     )
 }
@@ -93,7 +113,7 @@ private fun SwitchPreview() {
             checked = switchState,
             title = { Text("Enable Something") },
             subtitle = { PreferenceSubtitle(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.") },
-            leadingIcon = { Icon(painterResource(id = R.drawable.ic_build), contentDescription = "Build") },
+            //leadingIcon = { Icon(painterResource(id = R.drawable.ic_build), contentDescription = "Build") },
             onClick = {
                 switchState = it
             }
